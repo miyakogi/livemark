f1263fbec16b6287b165e11f5d4623ad39d4daf4
miyakogi <miyako.dev@gmail.com>
2016-02-27 20:54:30 +0900

    Add test for is_empty_text_node

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -5,6 +5,7 @@ from syncer import sync
 
 from livemark.diff import is_same_node, next_noempty, find_diff_node, is_empty_text_node
 
+from wdom.node import Text
 from wdom.tests.util import TestCase
 from wdom.parser import parse_html
 
@@ -45,6 +46,33 @@ class TestSameNode(TestCase):
         self.assertFalse(is_same_node(self.node1, self.t_node1))
         self.assertFalse(is_same_node(self.node2, self.t_node2))
         self.assertFalse(is_same_node(self.node3, self.t_node1))
+
+
+class EmptyTextText(TestCase):
+    def setUp(self):
+        self.node6 = parse_html('<a></a>').firstChild
+        self.node7 = parse_html('<img>').firstChild
+
+    def test_empty_string(self):
+        self.assertTrue(is_empty_text_node(Text('')))
+
+    def test_spaces_string(self):
+        self.assertTrue(is_empty_text_node(Text(' ')))
+
+    def test_newline_string(self):
+        self.assertTrue(is_empty_text_node(Text('\n')))
+
+    def test_newline_space_string(self):
+        self.assertTrue(is_empty_text_node(Text(' \n ')))
+
+    def test_newline_fullspace_string(self):
+        self.assertTrue(is_empty_text_node(Text('　\n　')))  # full-width space
+
+    def test_empty_tag(self):
+        self.assertFalse(is_empty_text_node(self.node6))
+
+    def test_empty_nonclose_tag(self):
+        self.assertFalse(is_empty_text_node(self.node7))
 
 
 class TextFindDiffNode(TestCase):
