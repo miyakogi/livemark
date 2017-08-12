9ebc7dc85721af69e536cb523a6f08a5390e8ec0
miyakogi <miyako.dev@gmail.com>
2017-08-12 18:11:42 +0900

    Add browser mode and remove auto scroll

--- livemark/__main__.py
+++ livemark/__main__.py
@@ -6,24 +6,20 @@ from os import path
 import json
 import asyncio
 
-from tornado import web  # type: ignore
-
-from pygments.styles import STYLE_MAP
+from pygments.styles import STYLE_MAP  # type: ignore
 from pygments.formatters import HtmlFormatter  # type: ignore
 
 current_dir = path.dirname(__file__)
 sys.path.insert(0, path.dirname(current_dir))
 sys.path.insert(0, path.join(path.dirname(current_dir), 'wdom'))
 
-from wdom.options import config, parser, parse_command_line
-from wdom.tag import Div, Style, H2, Script, WdomElement
-from wdom.document import get_document
-from wdom.server import start_server, add_static_path
-from wdom.parser import parse_html
-from wdom.util import install_asyncio
+from wdom.options import config, parser, parse_command_line  # noqa: F401
+from wdom.tag import Div, Style, H2, Script, WdomElement, RawHtmlNode  # noqa: F401
+from wdom.document import get_document  # noqa: F401
+from wdom.server import start_server, add_static_path  # noqa: F401
+from wdom.util import install_asyncio  # noqa: F401
 
-from livemark.diff import find_diff_node
-from livemark.converter import convert
+from livemark.converter import convert  # noqa: F401
 
 
 cursor_move_js = '''
@@ -52,84 +48,50 @@ class Preview(Div):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.tlist = []
-        self.dom_tree = None
+        self.current_view = H2('LiveMark is running...', parent=self)
 
     def data_received(self, data):
         line = data['line']
         event = data['event']
         if event == 'update':
             self.tlist = data['text']
-            _task = self.update_preview
+            self.update_preview(line)
         elif event == 'move':
-            _task = self.move_cursor
+            self.move(data['command'].lower())
         else:
             raise ValueError('Get unknown event: {}'.format(event))
-        _task(line)
 
     def update_preview(self, line):
         html = self.convert_to_html(self.tlist)
         self.mount_html(html)
-        self.move_cursor(line)
 
     def convert_to_html(self, tlist):
         md = '\n'.join(tlist)
         return convert(md)
 
     def mount_html(self, html):
-        fragment = parse_html(html)
-        self.dom_tree = fragment.cloneNode(True)
-        if self.length < 1:
-            self.appendChild(fragment)
-        else:
-            diff = find_diff_node(self, fragment)
-            for _i in diff['inserted']:
-                self.insertBefore(_i[1], _i[0])
-            for _d in diff['deleted']:
-                self.removeChild(_d)
-            for _a in diff['appended']:
-                self.appendChild(_a)
+        old_view = self.current_view
+        self.current_view = RawHtmlNode(html)
+        self.replaceChild(self.current_view, old_view)
 
-    def move_cursor(self, line):
-        if line < 0:
-            return
-
-        blank_lines = 0
-        i = 1
-        while i < line and i < len(self.tlist):
-            if self.tlist[i] == '' and self.tlist[i - 1] == '':
-                blank_lines += 1
-            i += 1
-        cur_line = line - blank_lines
-
-        node_n = 0
-        if self.dom_tree is not None and self.ownerDocument:
-            _l = 0
-            elm = self.dom_tree.firstChild
-            while elm is not None:
-                if elm.nodeName != '#text':
-                    node_n += 1
-                _l += elm.textContent.count('\n')
-                if _l >= cur_line:
-                    break
-                elm = elm.nextSibling
-
-            if node_n >= len(self):
-                top_node = self.lastChild
-            else:
-                top_node = self.childNodes[node_n - 1]
-            if top_node is not None:
-                if isinstance(top_node, WdomElement):
-                    self.move_to(top_node.rimo_id)
-                else:
-                    while top_node is not None:
-                        top_node = top_node.previousSibling
-                        if isinstance(top_node, WdomElement):
-                            self.move_to(top_node.rimo_id)
-                            break
-
-    def move_to(self, id):
-        script = 'moveToElement("{}")'.format(id)
-        self.js_exec('eval', script)
+    def move(self, cmd):
+        if cmd == 'top':
+            self.exec('window.scrollTo(0, 0)')
+        elif cmd == 'bottom':
+            self.exec(
+                'window.scrollTo(0, document.body.getClientRects()[0].height)')
+        elif cmd == 'page_up':
+            self.exec('window.scrollBy(0, - window.innerHeight * 0.9)')
+        elif cmd == 'page_down':
+            self.exec('window.scrollBy(0, window.innerHeight * 0.9)')
+        elif cmd == 'half_up':
+            self.exec('window.scrollBy(0, - window.innerHeight * 0.45)')
+        elif cmd == 'half_down':
+            self.exec('window.scrollBy(0, window.innerHeight * 0.45)')
+        elif cmd == 'up':
+            self.exec('window.scrollBy(0, -50)')
+        elif cmd == 'down':
+            self.exec('window.scrollBy(0, 50)')
 
 
 class SocketServer(object):
@@ -190,7 +152,6 @@ def main():
     script = Script(parent=doc.body)
     script.textContent = cursor_move_js
     preview = Preview(parent=doc.body, class_='container')
-    preview.appendChild(H2('LiveMark is running...'))
     for _d in _user_static_dirs:
         add_static_path(_d, _d)
     web_server = start_server()
